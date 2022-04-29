c62c76f8385c7bff218d192f9e0df53540086d9a
solomon igori <igorisolomon@gmail.com>
2022-04-29 11:27:51 +0100

    created the flie structure

---
+++ src/login/Login.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from "../assets/img/logo.svg";
+
+import "./Login.css"
+
+const Login = () => {
+  return (
+    <div>
+      <div className="login-container">
+        <img src={logo} alt="logo" />
+      </div>
+    </div>
+  )
+}
+
+export default Login
